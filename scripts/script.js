91fff1a7fbf926b11848a83e1b246b0e2c244079
Vegard <92518188+VegardGronas@users.noreply.github.com>
2023-10-21 17:54:28 +0200

    Updated hambuger menu

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.getElementById("nav-toggle").addEventListener("click", function () {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+});

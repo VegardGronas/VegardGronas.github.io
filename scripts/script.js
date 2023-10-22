88d4bcf11f625f0a42fd1599e698bee29ec74a34
Vegard <92518188+VegardGronas@users.noreply.github.com>
2023-10-22 10:38:21 +0200

    Updated index page

--- scripts/script.js
+++ scripts/script.js
@@ -3,36 +3,49 @@ document.getElementById("nav-toggle").addEventListener("click", function () {
     navLinks.classList.toggle("active");
 });
 
+// Get all card elements
+const cards = document.querySelectorAll('.card');
 
-// Get all elements with class 'image-modal'
-const imageModals = document.querySelectorAll('.image-modal');
+// Add click event listener to each card
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        const title = card.querySelector('h2').textContent;
 
-// Add click event listener to each image modal
-imageModals.forEach((imageModal) => {
-    imageModal.addEventListener('click', function() {
-        const modalId = this.getAttribute('data-modal');
-        const modal = document.getElementById(modalId);
-        const modalImage = modal.querySelector('.modal-content');
-        modal.style.display = 'block';
-        modalImage.src = this.src;
+        // Get all p tags within the card
+        const descriptionElements = card.querySelectorAll('p');
 
-        // Add a click event listener to the modal container to close it when clicking outside the image
-        modal.addEventListener('click', function(event) {
-            if (event.target === modal) {
-                modal.style.display = 'none';
-            }
+        const imgElement = card.querySelector('img');
+        const imageUrl = imgElement ? imgElement.src : null;
+
+        // Create an empty string to store the combined text content of all p tags
+        let description = '';
+
+        // Loop through the p tags and concatenate their text content with HTML line breaks
+        descriptionElements.forEach((p) => {
+            description += p.textContent + '<br>';
         });
+
+        document.getElementById('overlay-title').textContent = title;
+        document.getElementById('overlay-description').innerHTML = description; // Use innerHTML to interpret the <br> tags as line breaks
+        if (imageUrl) {
+            document.getElementById('overlay-image').src = imageUrl;
+        }
+        else{
+            document.getElementById('overlay-image').src = "";
+        }
+        document.getElementById('overlay').style.display = 'block';
     });
 });
 
-// Get all close buttons with class 'close'
-const closeModalButtons = document.querySelectorAll('.close');
+// Add a click event listener to the overlay
+document.getElementById('overlay').addEventListener('click', (event) => {
+    // Check if the clicked element is the overlay itself
+    if (event.target.id === 'overlay') {
+        document.getElementById('overlay').style.display = 'none';
+    }
+});
 
-// Add click event listener to each close button
-closeModalButtons.forEach((closeButton) => {
-    closeButton.addEventListener('click', function() {
-        const modalId = this.getAttribute('data-modal-close');
-        const modal = document.getElementById(modalId);
-        modal.style.display = 'none';
-    });
+// Close the overlay when the close button is clicked
+document.getElementById('close-overlay').addEventListener('click', () => {
+    document.getElementById('overlay').style.display = 'none';
 });
